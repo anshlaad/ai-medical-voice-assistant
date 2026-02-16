768fa43d712edb89ba04503e699921b4ff5e1710
Ansh Laad <laadansh765@gmail.com>
2026-02-17 02:05:11 +0530

    Fixed OpenRouter key issue

--- ai-medical-voice-agent/config/openAi.tsx
+++ ai-medical-voice-agent/config/openAi.tsx
@@ -1,6 +1,6 @@
-import OpenAI from "openai"
+import OpenAI from "openai";
 export const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENROUTER_API_KEY,
  
-})
+});
